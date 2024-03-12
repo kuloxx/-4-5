20d770c7254f9362898fe4b44bc51a3f543297c3
kuloxx <162123523+kuloxx@users.noreply.github.com>
2024-03-12 08:36:44 +0300

    лаба 4

---
+++ лаба4.py
@@ -0,0 +1,5 @@
+password = int(input('Введите пароль: '))
+while password != 1235:
+    print('Пароль не верен! ')
+    password = int(input('Введите пароль еще раз: '))
+print('Пароль верный! Добро пожаловать.')
